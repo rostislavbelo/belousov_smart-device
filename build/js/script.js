7eacd7dbc4de3997fca718fded0800dd7a7a5601
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2021-11-20 14:18:32 +0300

    добавляет верстку шапки с главным разделом

---
+++ build/js/script.js
@@ -0,0 +1,36 @@
+/* (function () {
+  const buttonMenu = document.querySelector('.mobile-menu__button');
+  const menuActivate = document.querySelector('.mobile-menu__items');
+  const hidden = document.querySelectorAll('.mobile-hidden');
+  const itemMenu = document.querySelectorAll('.main-nav__item-content');
+
+  if (buttonMenu && menuActivate) {
+    buttonMenu.classList.remove('mobile-menu__button--active');
+    menuActivate.classList.remove('mobile-menu__items--active');
+    buttonMenu.classList.remove('mobile-menu__button--hidden');
+
+    buttonMenu.addEventListener('click', () => {
+      menuActivate.classList.toggle('mobile-menu__items--active');
+      buttonMenu.classList.toggle('mobile-menu__button--active');
+
+      if (hidden) {
+        hidden.forEach((el) => {
+          el.classList.toggle('visually-hidden');
+        });
+      }
+    });
+
+    itemMenu.forEach((item) => {
+      item.addEventListener('click', () => {
+        if (hidden) {
+          hidden.forEach((el) => {
+            el.classList.remove('visually-hidden');
+          });
+        }
+        buttonMenu.classList.remove('mobile-menu__button--active');
+        menuActivate.classList.remove('mobile-menu__items--active');
+      });
+    });
+  }
+})();
+ */
